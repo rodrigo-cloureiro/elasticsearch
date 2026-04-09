5890e3fab9e054c2929e92327cc8a10353e79064
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2026-04-09 00:35:32 -0300

    atualiza ProdutoQueryService com a construcao direta de queries Elasticsearch

--- src/main/java/br/com/rodrigo/elasticsearch/service/ProdutoQueryService.java
+++ src/main/java/br/com/rodrigo/elasticsearch/service/ProdutoQueryService.java
@@ -1,6 +1,10 @@
 package br.com.rodrigo.elasticsearch.service;
 
-import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
+import co.elastic.clients.elasticsearch._types.aggregations.AggregationRange;
+import co.elastic.clients.elasticsearch._types.aggregations.AverageAggregation;
+import co.elastic.clients.elasticsearch._types.aggregations.RangeAggregation;
+import co.elastic.clients.elasticsearch._types.query_dsl.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -8,6 +12,7 @@ import java.util.List;
 @Component
 public class ProdutoQueryService {
 
+    /*
     public Query match(String field, String value) {
         return Query.of(q -> q
                 .match(m -> m
@@ -55,5 +60,76 @@ public class ProdutoQueryService {
                         )
                 )
         );
+    }
+    */
+
+    // ============================================================================================================== \\
+
+    public MatchQuery matchQuery(String field, String value) {
+        return MatchQuery.of(q -> q
+                .field(field)
+                .query(value)
+        );
+    }
+
+    public MatchPhraseQuery matchPhraseQuery(String field, String value) {
+        return MatchPhraseQuery.of(q -> q
+                .field(field)
+                .query(value)
+        );
+    }
+
+    public FuzzyQuery fuzzyQuery(String field, String value) {
+        return FuzzyQuery.of(q -> q
+                .field(field)
+                .value(value)
+                .fuzziness("AUTO")
+        );
+    }
+
+    public MultiMatchQuery multiMatchQuery(List<String> fields, String value) {
+        return MultiMatchQuery.of(q -> q
+                .fields(fields)
+                .query(value)
+        );
+    }
+
+    public NumberRangeQuery numberRangeQuery(String field, double min, double max) {
+        return NumberRangeQuery.of(q -> q
+                .field(field)
+                .gte(min)
+                .lte(max)
+        );
+    }
+
+    public TermQuery termQuery(String field, String value) {
+        return TermQuery.of(q -> q
+                .field(field)
+                .value(value)
+        );
+    }
+
+    public Aggregation termsAggregation(String field) {
+        return Aggregation.of(a -> a
+                .terms(t -> t
+                        .field(field)
+                )
+        );
+    }
+
+    public AverageAggregation averageAggregation(String field) {
+        return AverageAggregation.of(a -> a
+                .field(field)
+        );
+    }
+
+    public RangeAggregation rangeAggregation(String field, List<AggregationRange> ranges) {
+        return Aggregation.of(a -> a
+                        .range(RangeAggregation.of(r -> r
+                                .field(field)
+                                .ranges(ranges))
+                        )
+                )
+                .range();
     }
 }
