e95aeadfd7160a12310582a35e2fb4d89af3e302
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2026-04-09 00:29:56 -0300

    remove DTOs nao utilizados

--- src/main/java/br/com/rodrigo/elasticsearch/dto/CategoriaAggregation.java
+++
@@ -1,7 +0,0 @@
-package br.com.rodrigo.elasticsearch.dto;
-
-public record CategoriaAggregation(
-        String categoria,
-        long quantidade
-) {
-}
--- src/main/java/br/com/rodrigo/elasticsearch/dto/RaridadeAggregation.java
+++
@@ -1,7 +0,0 @@
-package br.com.rodrigo.elasticsearch.dto;
-
-public record RaridadeAggregation(
-        String raridade,
-        long quantidade
-) {
-}
